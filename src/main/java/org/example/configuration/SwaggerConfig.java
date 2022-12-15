af79fe806140f901b97b56900ad7ad9993d166b7
PattimuraDev <dwisatriapatra2000@gmail.com>
2022-12-15 14:04:56 +0700

    change swagger server

--- src/main/java/org/example/configuration/SwaggerConfig.java
+++ src/main/java/org/example/configuration/SwaggerConfig.java
@@ -6,9 +6,13 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,6 +21,10 @@ public class SwaggerConfig {
                            @Value("v1.0.0") String appVersion
     ) {
         final String securitySchemeName = "bearerAuth";
+        Server server = new Server();
+        server.setUrl("https://microserviceuserchallenge7-production.up.railway.app/");
+        List<Server> listOfServer = new ArrayList<>();
+        listOfServer.add(server);
 
         return new OpenAPI()
                 .info(new Info()
@@ -29,6 +37,7 @@ public class SwaggerConfig {
                                 .url("http://springdoc.org")
                         )
                 )
+                .servers(listOfServer)
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName)
                 )
